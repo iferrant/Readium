711ec33c569794742812d85d641168e1df1f1855
iferrant <ivan@ivanferrant.com>
2017-04-07 00:04:47 +0200

    Add basic interface
    - Create navitagion bar
    - Add example content
    - First approach of login screen

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 import webapp2
+from webapp2_extras import jinja2
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("base.html", **{}))
