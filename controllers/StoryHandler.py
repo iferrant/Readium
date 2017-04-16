1c7623d55165c55279ca2cc7ff365c893e2c200d
iferrant <ivan@ivanferrant.com>
2017-04-16 18:30:37 +0200

    Read stories
    When the user clicks on a story from the story list, he can read it.

--- controllers/StoryHandler.py
+++ controllers/StoryHandler.py
@@ -1,8 +1,25 @@
 import webapp2
 from webapp2_extras import jinja2
+from google.appengine.ext import ndb
 
 
 class StoryHandler(webapp2.RequestHandler):
     def get(self):
         jinja = jinja2.get_jinja2(app=self.app)
         self.response.write(jinja.render_template("write_story.html", **{}))
+
+
+class ReadStoryHandler(webapp2.RequestHandler):
+    def get(self):
+        try:
+            id = self.request.GET['id']
+
+        except:
+            id = None
+
+        story = ndb.Key(urlsafe=id).get()
+        read_values = {
+            "story": story
+        }
+        jinja = jinja2.get_jinja2(app=self.app)
+        self.response.write(jinja.render_template("read_story.html", **read_values))
--- main.py
+++ main.py
@@ -17,10 +17,11 @@
 import webapp2
 from controllers.PostListHandler import PostListHandler
 from controllers.LoginHandler import LoginHandler
-from controllers.StoryHandler import StoryHandler
+from controllers.StoryHandler import StoryHandler, ReadStoryHandler
 
 app = webapp2.WSGIApplication([
     ('/', PostListHandler),
     ('/login', LoginHandler),
-    ('/write_story', StoryHandler)
+    ('/write_story', StoryHandler),
+    ('/read_story', ReadStoryHandler)
 ], debug=True)
