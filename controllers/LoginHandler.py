7f7dec5ccc7144a893a03e52034215b151216380
iferrant <ivan@ivanferrant.com>
2017-04-21 21:29:40 +0200

    Refactor story_card to use just one container

--- controllers/LoginHandler.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-from webapp2_extras import jinja2
-
-
-class LoginHandler(webapp2.RequestHandler):
-    def get(self):
-        jinja = jinja2.get_jinja2(app=self.app)
-        self.response.write(jinja.render_template("login.html", **{}))
